c9c8accc1755ba2eb95303bf62a8f42c79b74c51
cesardy <56207071+cesardy@users.noreply.github.com>
2025-06-23 16:36:00 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,56 @@
+# Bienvenido al repositorio OFC de Rembot 😸❤️
+
+Si gustas un bot para tu grupo puedes hablar con mi owner 🐢 
+
+![Logo de Rembot](Grupo.jpg)
+
+Bot con más de 300 comandos 👑
+
+# Redes oficiales del bot
+
+**Grupo OFC**
+
+
+[![](https://img.shields.io/badge/WhatsApp-25D366?style=for-the-badge&logo=whatsapp&logoColor=white)](https://chat.whatsapp.com/E9j3js91NjYAYQvGW6Et2T)
+
+
+
+**canal OFC**
+
+[![](https://img.shields.io/badge/WhatsApp%20Canal-25D366?style=for-the-badge&logo=whatsapp&logoColor=white)](https://whatsapp.com/channel/0029VapASNA9cDDT9yfhXr30)
+
+
+# Contactame
+
+
+**Owner 1**
+
+[![](https://img.shields.io/badge/WhatsApp-25D366?style=for-the-badge&logo=whatsapp&logoColor=white)](https://wa.me/+527421168105)
+
+
+**Owner 2**
+
+[![](https://img.shields.io/badge/WhatsApp-25D366?style=for-the-badge&logo=whatsapp&logoColor=white)](https://wa.me/+34613772401)
+
+
+## Instalación por Cloud Shell
+
+```bash
+git clone https://github.com/TOBI-12/Rembot
+```
+
+```bash
+cd Rembot
+```
+
+```bash
+npm install
+```
+```bash
+npm start
+```
+
+
+
+
+¡Espero que disfrutes usando Rembot! 😸❤️
---
+++ config.js
@@ -0,0 +1,97 @@
+import { watchFile, unwatchFile } from 'fs' 
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
+import cheerio from 'cheerio'
+import fetch from 'node-fetch'
+import axios from 'axios'
+import moment from 'moment-timezone' 
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+global.owner = [
+   ['573176084059', 'Creador cesardyi💙', true],
+   ['595994384940', 'Comprador', true],
+   ['5212731590195', 'Gestion White', true],
+   ['584148256527', 'Colaborador Diomar', true],
+   ['5491133980616', 'Kara', true],
+   ['51971867199', 'Dev Senna', true],
+   ['584241836217', 'Dev Prak', true],
+   ['519168382008', 'Dev Darkcore', true],
+   ['573215751237', 'CrowBot OFC1', true],
+]
+
+global.creadorbot = [
+   ['57317084059', 'cesardy', true],
+]
+  
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.mods = []
+global.prems = []
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.packname = '【✧】𝐂𝐡𝐚𝐞 𝐡𝐚𝐞-𝐢𝐧࿐'
+global.botname = '(✦◠‿◠)𝐂𝐡𝐚𝐞 𝐡𝐚𝐞-𝐢𝐧'
+global.wm = '⏤͟͟͞͞⋆⬪࣪ꥈ🌟★ ׄ ꒱ 𝐂𝐡𝐚𝐞 𝐡𝐚𝐞-𝐢𝐧 ୭'
+global.author = '꒷꒦𖥻⭕cesardy𓆪'
+global.dev = '© ⍴᥆ᥕᥱrᥱძ ᑲᥡ ⭕cesardy'
+global.errorm = '𝐄𝐫𝐫𝐨𝐫: ${error.message}'
+global.errorm2 = '✰ 𝐀 𝐎𝐜𝐮𝐫𝐫𝐢𝐝𝐨 𝐔𝐧 𝐄𝐫𝐫𝐨𝐫, 𝐕𝐮𝐞𝐥𝐯𝐞 𝐌𝐚𝐬 𝐓𝐚𝐫𝐝𝐞 (৹˃̵﹏˂̵৹)'
+global.resp = '𝐀𝐪𝐮𝐢 𝐓𝐢𝐞𝐧𝐞 𝐒𝐮 𝐏𝐞𝐝𝐢𝐝𝐨 〜(^∇^〜)'
+global.espera = '✰ 𝐄𝐬𝐩𝐞𝐫𝐚 𝐔𝐧 𝐌𝐨𝐦𝐞𝐧𝐭𝐨, 𝐄𝐬𝐭𝐚𝐦𝐨 𝐄𝐧𝐯𝐢𝐚𝐧𝐝𝐨 𝐒𝐮 𝐏𝐞𝐝𝐢𝐝𝐨 (✿◠‿◠)'
+global.nombrebot = '𝐂𝐡𝐚𝐞 𝐡𝐚𝐞-𝐢𝐧'
+global.textbot = `༻𝐂𝐡𝐚𝐞 𝐡𝐚𝐞-𝐢𝐧༺`
+global.vs = '2.1.0'
+global.publi = '✰𝐒𝐢𝐠𝐮𝐞 𝐄𝐥 𝐂𝐚𝐧𝐚𝐥👇'
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+// Set De Rem-Bot
+
+global.welmss = 'Bienvenido a'
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.imagen1 = fs.readFileSync('./Menu.jpg')
+global.fantasyVid = ['./media/menus/Menuvid1.mp4', './media/menus/Menuvid2.mp4', './media/menus/Menuvid3.mp4']
+global.imagen2 = fs.readFileSync('./Menu2.jpg')
+global.imagen3 = fs.readFileSync('./Menu3.jpg')
+global.welcome = fs.readFileSync('./media/welcome.jpg')
+global.adios = fs.readFileSync('./media/adios.jpg')
+global.catalogo = fs.readFileSync('./storage/img/catalogo1.jpg')
+global.crowurl = fs.readFileSync('./storage/img/crowurl.jpg')
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+global.mashach = ''
+global.mashac = 'wa.me/573176084059*/n• *wa.me/573176084059*/n• *wa.me/573176084059*'
+global.repobot = 'https://github.com/cesardy'
+global.grupo = ''
+global.comu = 'j'
+global.channel = ''
+global.insta = ''
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "54387657797-120363317332020195@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: '👑【✫𝚃𝙴𝙰𝙼  乂 cesardy✫】🎩', orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.cheerio = cheerio
+global.fs = fs
+global.fetch = fetch
+global.axios = axios
+global.moment = moment        
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.multiplier = 69 
+global.maxwarn = '2' // máxima advertencias
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  import(`${file}?update=${Date.now()}`)
+})
